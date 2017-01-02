f758772ef3d9210487e305eee525c3ca685afd41
Ajith <ajith.rahu@gmail.com>
2017-01-01 22:27:04 -0500

    Add TodoApp Component

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <p>Happy New Year Fam</p>
+      </div>
+    )
+  }
+});
+
+module.exports = TodoApp;
