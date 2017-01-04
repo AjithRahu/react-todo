58921007e3cc52641d5a666322152422f48e4be2
Ajith <ajith.rahu@gmail.com>
2017-01-03 23:50:35 -0500

    Refactor TodoSearch to work with redux store

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -1,96 +1,30 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Provider} = require('react-redux');
+var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
+var $ = require('jquery');
 
-var TodoAPI = require('TodoAPI');
+var configureStore = require('configureStore');
+var TodoApp = require('TodoApp');
+import TodoList from 'TodoList';
 
-describe('TodoAPI', () => {
-  beforeEach(() => {
-    localStorage.removeItem('todos');
-  });
-
+describe('TodoApp', () => {
   it('should exist', () => {
-    expect(TodoAPI).toExist();
+    expect(TodoApp).toExist();
   });
 
-  describe('setTodos', () => {
-    it('should set valid todos array', () => {
-      var todos = [{
-        id: 23,
-        test: 'test all files',
-        completed: false
-      }];
-      TodoAPI.setTodos(todos);
+  it('should render TodoList', () => {
+    var store = configureStore.configure();
+    var provider = TestUtils.renderIntoDocument(
+      <Provider store={store}>
+        <TodoApp/>
+      </Provider>
+    );
 
-      var actualTodos = JSON.parse(localStorage.getItem('todos'));
+    var todoApp = TestUtils.scryRenderedComponentsWithType(provider, TodoApp)[0]
+    var todoList = TestUtils.scryRenderedComponentsWithType(todoApp, TodoList);
 
-      expect(actualTodos).toEqual(todos);
-    });
-
-    it('should not set invalid todos array', () => {
-      var badTodos = {a: 'b'};
-      TodoAPI.setTodos(badTodos);
-
-      expect(localStorage.getItem('todos')).toBe(null);
-    });
-  });
-
-  describe('getTodos', () => {
-    it('should return empty array for bad localstorage data', () => {
-      var actualTodos = TodoAPI.getTodos();
-      expect(actualTodos).toEqual([]);
-    });
-
-    it('should return todo if valid array in localstorage', () => {
-      var todos = [{
-        id: 23,
-        test: 'test all files',
-        completed: false
-      }];
-
-      localStorage.setItem('todos', JSON.stringify(todos));
-      var actualTodos = TodoAPI.getTodos();
-
-      expect(actualTodos).toEqual(todos);
-    });
-  });
-
-  describe('filterTodos', () => {
-    var todos = [{
-      id: 1,
-      text: 'Some text here',
-      completed: true
-    },{
-      id: 2,
-      text: 'Other text here',
-      completed: false
-    },{
-      id: 3,
-      text: 'Some text here',
-      completed: true
-    }];
-
-    it('should return all items if showCompleted is true', () => {
-      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
-      expect(filteredTodos.length).toBe(3);
-    });
-
-    it('should return non-completed todos when showCompleted is false', () => {
-      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
-      expect(filteredTodos.length).toBe(1);
-    });
-
-    it('should sort by completed status', () => {
-      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
-      expect(filteredTodos[0].completed).toBe(false);
-    });
-
-    it('should filter todos by searchText', () => {
-      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
-      expect(filteredTodos.length).toBe(2);
-    });
-
-    it('should return all todos if searchText is empty', () => {
-      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
-      expect(filteredTodos.length).toBe(3);
-    });
+    expect(todoList.length).toEqual(1);
   });
 });
